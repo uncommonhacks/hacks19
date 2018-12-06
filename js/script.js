1a4d57e8d428427f4a75e622df2f9623e90dbc3e
George Saieed <saieed.george0@gmail.com>
2018-12-05 23:43:58 -0600

    finish v1 of site

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
-let toggleFAQ = function(ind) {
-    if (ind < 1 || ind > 6) {
-	return;
-    }
-
-    let collapsedFAQSection = document.getElementById("faq-" + ind);
-
-    // Add or remove the faq-collapsed class
-    if (collapsedFAQSection.classList.contains("faq-collapsed")) {
-	collapsedFAQSection.classList.remove("faq-collapsed");
-    } else {
-	collapsedFAQSection.classList.add("faq-collapsed");
-    }
-
-    console.log("Toggled" + ind);
-};
-
-let main = function() {
-    // If javascript is enabled, set all of the FAQs to collapsed by default.
-    // If javascript is not enabled, they will be uncollapsed by default.
-    var i;
-    for (i = 1; i < 9; i++){
-	toggleFAQ(i)
-    }
-
-};
-
-main();
+$(document).ready(function(){
+    $('.faqq').click(function() {
+        if ($(this).next().css("display") == "block") {
+            $(".off").css("display", "none");
+        }
+        else {
+            $(".off").css("display", "none");
+            $(this).next().css("display", "block");
+        }
+    });
+ });
