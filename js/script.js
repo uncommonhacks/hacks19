4b14b6d6d11ac6a5335e89a5eafab3921ccaee93
Michael Noronha <michaeltnoronha@gmail.com>
2018-12-18 02:54:22 -0600

    Relative without javascript

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let displayAnswer = function(e) {
-  let next = e.nextElementSibling;
-  if (next.style.display == "block") {
-    next.style.display = "none";
-  }
-  else {
-    next.style.display = "block";
-  }
-};
